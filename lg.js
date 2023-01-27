04c1de7740695143e2c0489ca3f65f6d1c44c037
vitalik1970 <ytcenko1970@gmail.com>
2023-01-27 18:45:43 +0200

    Addition in file lang

---
+++ lg.js
@@ -0,0 +1,50 @@
+let fs = require('fs');
+const { off } = require('process');
+
+//fs.readFile('de.json',(err, data) => {
+	//if (err) throw err;
+	  // let arr = data.toString();
+	   //arr = JSON.parse(arr);
+	    // for (let i in arr){}})
+fs.readdir('html', function (err, file) {
+		
+		 for (let i=1; i < file.length; i++){
+			console.log(file);
+		};//ror
+		//let arrHT = file[].replace(`{$`+ i + `}`, arr[i]);
+	//fs.mkdir('de', () => {});
+	//	fs.writeFile('de/index.html',arrHT, (err) => {
+	//if (err) {
+	 // console.error(err)
+	// return
+	//}})
+	
+ // })
+	
+//})	
+	//console.log(file);
+	})//readdir		
+		 
+
+//fs.readFile('Html/index_key.html',(err, dataHT) => {
+	//if (err) throw err;
+	   //let arrHT = dataHT.toString();
+			
+	//for (let i in arr){
+	//	arrHT = arrHT.replace(`{$`+ i + `}`, arr[i]);
+		
+	//};
+	//fs.mkdir('de', () => {});
+	
+
+
+	//fs.writeFile('de/index.html',arrHT, (err) => {
+	//if (err) {
+	 // console.error(err)
+	// return
+	//}})
+	
+ // })
+//}
+
+//})
